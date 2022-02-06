9c71111e49b1c52be7bdba79566399914c5da1dc
Kasia Fahey <kasia.mcmahon@gmail.com>
2022-02-06 11:32:54 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import './App.css';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+import Form from './Form.js'
+
+
+
+function CheckboxExample() {
+  return (
+    <div>
+      <Checkbox
+        />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Form />
+        <TextField
+            variant="outlined"
+            color="secondary"
+            label="Text Field"
+          />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +43,16 @@ function App() {
         >
           Learn React
         </a>
+
+
+        <Button onClick={()=>alert('You clicked a button')}
+        size="medium"
+        variant="contained"
+        color="secondary">
+          Hello React</Button>
+
+
+
       </header>
     </div>
   );
---
+++ src/Form.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
+const defaultValues = {
+    name: "",
+    age: 20,
+};
+
+const Form = () => {
+    const [formValues, setFormValues] = useState(defaultValues);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({
+            ...formValues,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formValues);
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <Grid container alignItems="center" justify="center" direction="column">
+                <Grid item>
+                    <TextField
+                        id="name-input"
+                        name="name"
+                        label="Name"
+                        type="text"
+                        value={formValues.name}
+                        onChange={handleInputChange}
+                    />
+                </Grid>
+                <Grid item>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={formValues.age}
+                            label="Age"
+                            onChange={handleInputChange}
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+
+                    </FormControl>
+                </Grid>
+
+
+                <Button variant="contained" color="primary" type="submit">
+                    Submit
+                </Button>
+
+            </Grid>
+        </form>
+    );
+};
+export default Form;
